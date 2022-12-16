cba10cc9c4e49b5343a09dbee2ecd102e8792f68
Maria De Sousa Ardiet <mdg.desousa@gmail.com>
2022-12-16 11:45:05 +0100

    fixed indentation

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -12,8 +12,8 @@ class LoginController extends AbstractController
     #[Route('/login', name: 'app_login')]
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
-       $error = $authenticationUtils->getLastAuthenticationError();
-       $lastUsername = $authenticationUtils->getLastUsername();
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('login/index.html.twig', [
             'controller_name' => 'LoginController',
