0ed2cd69aa21b66941e1af59bb837057ee3c1783
Maria De Sousa Ardiet <mdg.desousa@gmail.com>
2022-12-20 13:47:33 +0100

    US-11 - affichage décision - partie avis + première décision

--- src/Entity/Contributor.php
+++ src/Entity/Contributor.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ContributorRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ContributorRepository::class)]
@@ -21,6 +23,14 @@ class Contributor
 
     #[ORM\ManyToOne(inversedBy: 'contributors')]
     private ?Implication $implication = null;
+
+    #[ORM\OneToMany(mappedBy: 'contributor', targetEntity: Contribution::class)]
+    private Collection $contributions;
+
+    public function __construct()
+    {
+        $this->contributions = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -59,6 +69,36 @@ class Contributor
     public function setImplication(?Implication $implication): self
     {
         $this->implication = $implication;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Contribution>
+     */
+    public function getContributions(): Collection
+    {
+        return $this->contributions;
+    }
+
+    public function addContribution(Contribution $contribution): self
+    {
+        if (!$this->contributions->contains($contribution)) {
+            $this->contributions->add($contribution);
+            $contribution->setContributor($this);
+        }
+
+        return $this;
+    }
+
+    public function removeContribution(Contribution $contribution): self
+    {
+        if ($this->contributions->removeElement($contribution)) {
+            // set the owning side to null (unless already changed)
+            if ($contribution->getContributor() === $this) {
+                $contribution->setContributor(null);
+            }
+        }
 
         return $this;
     }
