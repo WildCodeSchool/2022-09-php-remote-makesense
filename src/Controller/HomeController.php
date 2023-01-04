92ab8f5cc5a6998921c6cecd1099a3bdf5e31d64
axl-crz <acrozier15@icloud.com>
2023-01-04 17:54:27 +0100

    Mise en place de la page d'acceuil et préparation des filtres

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,31 @@
 
 namespace App\Controller;
 
+use App\Repository\DecisionRepository;
+use App\Repository\TimelineRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use App\Entity\Decision;
+use App\Entity\Timeline;
 
+#[IsGranted('ROLE_USER')]
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(DecisionRepository $decisionRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        $decisions = $decisionRepository->findAllByTimeline();
+
+        $userDecisions = $decisionRepository->findAllByUser($this->getUser());
+        $userContributions = $decisionRepository->findAllByContributor($this->getUser());
+
+        return $this->render('home/index.html.twig', [
+            'decision' => $decisions,
+            'userDecisions' => $userDecisions,
+            'userContributions' => $userContributions,
+        ]);
     }
 }
--- src/Repository/DecisionRepository.php
+++ src/Repository/DecisionRepository.php
@@ -3,8 +3,10 @@
 namespace App\Repository;
 
 use App\Entity\Decision;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @extends ServiceEntityRepository<Decision>
@@ -39,20 +41,59 @@ class DecisionRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Decision[] Returns an array of Decision objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('d')
-//            ->andWhere('d.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('d.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    /**
+     * @return Decision[] Returns an array of Decision objects
+     */
+    public function findAllByTimeline(): array
+    {
+        $queryBuilder = $this->createQueryBuilder('d')
+            ->join('d.timelines', 't')
+            ->where('t.startedAt > CURRENT_DATE()')
+            ->orderBy('t.startedAt', 'ASC')
+            ->addSelect('d')->addSelect('t')
+            ->getQuery();
+        return $queryBuilder->getResult();
+    }
+
+    public function findAllByUser(UserInterface $user): array
+    {
+        $queryBuilder = $this->createQueryBuilder('d')
+            ->join('d.timelines', 't')
+            ->join('d.user', 'u')
+            ->where('t.startedAt > CURRENT_DATE()')
+            ->andwhere('d.user = :user')
+            ->setParameter('user', $user)
+            ->orderBy('t.startedAt', 'ASC')
+            ->setMaxResults(3)
+            ->addSelect('d')
+            ->addSelect('t')
+            ->addSelect('u')
+            ->getQuery();
+        return $queryBuilder->getResult();
+    }
+
+    public function findAllByContributor(UserInterface $user): array
+    {
+        $queryBuilder = $this->createQueryBuilder('d')
+            ->join('d.contributors', 'c')
+            ->join('c.employee', 'e')
+            ->join('e.user', 'u')
+            ->join('d.timelines', 't')
+            ->where('u.id = :user')
+            ->andwhere('t.endedAt > CURRENT_DATE()')
+            ->andwhere("t.name = ':deadline pour donner son avis'")
+            ->orwhere("t.name = ':deadline pour entrer en conflit'")
+            ->setParameter('user', $user)
+            ->orderBy('t.endedAt', 'ASC')
+            ->setMaxResults(3)
+            ->addSelect('d')
+            ->addSelect('e')
+            ->addSelect('u')
+            ->addSelect('c')
+            ->addSelect('t')
+            ->getQuery();
+        return $queryBuilder->getResult();
+    }
 
 //    public function findOneBySomeField($value): ?Decision
 //    {
