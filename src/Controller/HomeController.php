a598471ae8cf54ac9c9bbeb1fccb24f736190c04
SkyriMeg <m-pilon@orange.fr  git config --global user.name SkyriMeggit config --global user.name Meggit config --global user.email m-pilon@orange.fr  git config --global user.email m-pilon@orange.fr>
2023-01-23 19:20:40 +0100

    US_30 OK + fix phrase inscription + fix icônes pour impacté ou expert

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,20 +4,20 @@ namespace App\Controller;
 
 use App\Repository\DecisionRepository;
 use App\Repository\TimelineRepository;
-use App\Repository\UserRepository;
+use App\Security\EmailVerifier;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use App\Entity\Decision;
-use App\Entity\Timeline;
 
 #[IsGranted('ROLE_USER')]
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(DecisionRepository $decisionRepository, TimeLineRepository $timelineRepository): Response
-    {
+    public function index(
+        DecisionRepository $decisionRepository,
+        TimeLineRepository $timelineRepository,
+    ): Response {
         $decisions = $decisionRepository->findAllByTimeline();
         $userDecisions = $decisionRepository->findFirstThreeByUser($this->getUser());
         $userContributions = $timelineRepository->findAllByContributor($this->getUser());
@@ -27,17 +27,5 @@ class HomeController extends AbstractController
             'userDecisions' => $userDecisions,
             'userContributions' => $userContributions,
         ]);
-    }
-
-    #[Route('/aboutus', name: 'app_about_us')]
-    public function aboutUs(): Response
-    {
-        return $this->render('home/about_us.html.twig');
-    }
-
-    #[Route('/legalmentions', name: 'app_legal_mentions')]
-    public function legalMentions(): Response
-    {
-        return $this->render('home/legal_mentions.html.twig');
     }
 }
