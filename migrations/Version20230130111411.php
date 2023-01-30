4a0cf177b19b52e05c39341e31588ff48a667a51
Alexandre Delmotte <alex.delmotte89@gmail.com>
2023-01-30 12:36:46 +0100

    US_37 fix

--- migrations/Version20230130111411.php
+++ migrations/Version20230130111411.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230128221706 extends AbstractMigration
+final class Version20230130111411 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,16 +20,12 @@ final class Version20230128221706 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE contributor DROP poster');
-        $this->addSql('ALTER TABLE employee ADD poster VARCHAR(255) DEFAULT NULL');
-        $this->addSql('ALTER TABLE user ADD poster VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE user CHANGE reset_token reset_token VARCHAR(100) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE employee DROP poster');
-        $this->addSql('ALTER TABLE contributor ADD poster VARCHAR(255) DEFAULT NULL');
-        $this->addSql('ALTER TABLE user DROP poster');
+        $this->addSql('ALTER TABLE user CHANGE reset_token reset_token VARCHAR(100) NOT NULL');
     }
 }
